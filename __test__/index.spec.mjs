fb9353dbd3014267edd660a812f74dae958051d3
Esurio <esurio@esurio1673.net>
2024-11-19 09:41:02 +0900

    testを通るように

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,12 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { decodeTime, isValid } from '../index.js'
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
+test('isValid', (t) => {
+  t.is(isValid('01ARYZ6S41TSV4RRFFQ69G5FAV'), true)
+  t.is(isValid('01ARYZ6S41TSV4RRFFQ69G5FA'), false)
+})
+
+test('decodeTime', (t) => {
+  t.is(decodeTime('01JCZW9GR1BTE6Z1SNGZ40A4XH'), 1731941679873)
 })
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 #![deny(clippy::all)]
 
+use std::str::FromStr;
+
 use napi::{Error, Status};
 
 #[macro_use]
 extern crate napi_derive;
+
+use rusty_ulid::{DecodingError, Ulid};
 
 const TIME_LEN: usize = 10;
 const RANDOM_LEN: usize = 16;
@@ -20,15 +24,12 @@ pub fn is_valid(id: String) -> bool {
 
 #[napi]
 pub fn decode_time(id: String) -> Result<i64, Error> {
-  let valid = is_valid(id.clone());
-  if valid == false {
+  if !is_valid(id.clone()) {
     return Err(Error::new(Status::InvalidArg, "Invalid ID".to_string()));
   }
 
-  let time_str = &id[..TIME_LEN];
-  let time = u64::from_str_radix(time_str, 32).map_err(|e| {
-    Error::new(Status::InvalidArg, format!("Failed to parse time: {}", e))
-  })?;
-
-  Ok(time as i64)
+  let ulid: Ulid = Ulid::from_str(&id)
+    .map_err(|e: DecodingError| Error::new(Status::InvalidArg, e.to_string()))?;
+  let timestamp = ulid.timestamp();
+  Ok(timestamp as i64)
 }
